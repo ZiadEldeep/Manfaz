d3d8fdd3d66e856b960cfd3688c4cc41c124e42a
ahmed15ayman7 <ahmed15ayman7ahmed2002@gmail.com>
2025-02-09 22:22:20 +0200

    update all  apis

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -26,4 +26,34 @@ const createCategory = async (req, res) => {
   }
 };
 
-module.exports = { getAllCategories, createCategory };
+// Update Category
+const updateCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, slug, description, status, sortOrder,imageUrl } = req.body;
+    if (!name || !slug || !description || !status || !sortOrder) {
+      return res.status(400).json({ error: 'Name, slug, description, status, and sortOrder are required' });
+    }
+    const updatedCategory = await prisma.category.update({
+      where: { id },
+      data: { name, slug, description, status, sortOrder,imageUrl },
+    });
+    res.status(200).json(updatedCategory);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Delete Category
+const deleteCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const category = await prisma.category.delete({
+      where: { id },
+    });
+    res.status(200).json(category);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+module.exports = { getAllCategories, createCategory, updateCategory, deleteCategory };
--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { getAllCategories, createCategory } = require('../controllers/categoryController');
+const { getAllCategories, createCategory, updateCategory, deleteCategory } = require('../controllers/categoryController');
 const router = express.Router();
 
 router.get('/', getAllCategories);
 router.post('/', createCategory);
+router.put('/:id', updateCategory);
+router.delete('/:id', deleteCategory);
 
 module.exports = router;
