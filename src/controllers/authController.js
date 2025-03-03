a5fd43a0ef54b33c396dbbad726a12c4817fca07
ahmed15ayman7 <ahmed15ayman7ahmed2002@gmail.com>
2025-03-04 00:41:04 +0200

    update worker and enum type

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -38,11 +38,10 @@ const register = async (req, res) => {
 
     const verificationCode = generateVerificationCode();
     const hashedPassword = await bcrypt.hash(password, 10);
-    const translatedName = await translate(name, { to: 'en' });
 
     const newUser = await prisma.user.create({
       data: { 
-        name: translatedName, 
+        name, 
         email, 
         phone, 
         password: hashedPassword,
