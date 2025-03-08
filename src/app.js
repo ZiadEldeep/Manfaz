9438c2edc6facf2038f7027fc0fcbb0113a796e6
ahmed15ayman7 <ahmed15ayman7ahmed2002@gmail.com>
2025-03-09 01:58:19 +0200

    update order

--- src/app.js
+++ src/app.js
@@ -24,8 +24,9 @@ const app = express();
 // Middleware
 app.use(bodyParser.json());
 app.use(cors({
-    origin: 'http://localhost:3000', // Allow requests from this origin
+    origin: ['http://localhost:3000',"http://manfaz.vercel.app/","https://manfaz-dashboard.vercel.app/"], // Allow requests from this origin
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true // Allow credentials if needed
 }));
 
