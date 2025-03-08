3747822de54a002698fcb50b8b5bba6239479d00
ahmed15ayman7 <ahmed15ayman7ahmed2002@gmail.com>
2025-03-08 15:32:50 +0200

    update dashboard

--- src/app.js
+++ src/app.js
@@ -23,20 +23,7 @@ const app = express();
 
 // Middleware
 app.use(bodyParser.json());
-const allowedOrigins = ['http://localhost:3000'];
-
-app.use(cors({
-  credentials: true,
-  origin: function (origin, callback) {
-    // Allow requests with no origin
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors({credentials: true, origin: process.env.CORS_ORIGIN}));
 
 // Routes
 app.use('/auth', authRoutes);
