06914cc85450b1ec898e64f60baaa89d9b8df825
ahmed15ayman7 <ahmed15ayman7ahmed2002@gmail.com>
2025-03-09 19:28:35 +0200

    update cors

--- src/app.js
+++ src/app.js
@@ -29,13 +29,13 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true // Allow credentials if needed
 }));
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", ['http://localhost:3000',"https://manfaz.vercel.app/","https://manfaz-dashboard.vercel.app/"]); 
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", ['http://localhost:3000',"https://manfaz.vercel.app/","https://manfaz-dashboard.vercel.app/"]); 
+//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//     res.header("Access-Control-Allow-Credentials", "true");
+//     next();
+// });
 // Routes
 app.use('/auth', authRoutes);
 app.use('/auth-admin', authAdminRoutes);
