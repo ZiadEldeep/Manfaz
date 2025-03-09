96cd9764894bd3426dc12827d2bd700a5bfa9b9b
ahmed15ayman7 <ahmed15ayman7ahmed2002@gmail.com>
2025-03-09 19:31:22 +0200

    update cors

--- src/app.js
+++ src/app.js
@@ -24,18 +24,18 @@ const app = express();
 // Middleware
 app.use(bodyParser.json());
 app.use(cors({
-    origin: ['http://localhost:3000',"https://manfaz.vercel.app/","https://manfaz-dashboard.vercel.app/"], // Allow requests from this origin
+    origin: ['http://localhost:3000',"https://manfaz.vercel.app","https://manfaz-dashboard.vercel.app"], // Allow requests from this origin
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true // Allow credentials if needed
 }));
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", ['http://localhost:3000',"https://manfaz.vercel.app/","https://manfaz-dashboard.vercel.app/"]); 
-//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//     res.header("Access-Control-Allow-Credentials", "true");
-//     next();
-// });
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", ['http://localhost:3000',"https://manfaz.vercel.app","https://manfaz-dashboard.vercel.app"]); 
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
 // Routes
 app.use('/auth', authRoutes);
 app.use('/auth-admin', authAdminRoutes);
