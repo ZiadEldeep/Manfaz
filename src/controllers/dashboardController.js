f7cb42373260a71af06a22d3ebb42eeba623d89b
ahmed15ayman7 <ahmed15ayman7ahmed2002@gmail.com>
2025-03-09 03:08:49 +0200

    update order

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -1,4 +1,4 @@
-const { prisma } = require("../prismaClient");
+const prisma = require('../prismaClient');
 const translate = require("translate-google");
 async function checkPermissions(employeeId) {
   try {
